b2c7f7b21af68b6ec42c13c0644b9a7944f933cf
Sam Amistoso <sam.amistoso@hypersense-software.com>
2025-03-18 18:47:14 +0800

    initial setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+// eslint-disable-next-line no-undef
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: 'all',
+};
